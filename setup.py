f87d2bce0898a09154abaf87059505d904371a3d
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2012-05-26 17:02:07 +0400

    Bumped version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='obraz',
-    version='0.1.1',
+    version='0.1.2',
     packages=['test'],
     py_modules=['obraz'],
     install_requires=['PyYAML', 'Jinja2', 'Markdown'],
