929b4d90d70f79a2fad960d22469d8262fa13842
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2018-10-06 18:21:21 +0300

    Modernized setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+import setuptools
 
 
 def all_data_files(path, excluded_dirs=()):
@@ -13,9 +13,26 @@ def all_data_files(path, excluded_dirs=()):
 with open('README.md', 'r') as fd:
     long_description = fd.read()
 
-setup(
+setuptools.setup(
     name='obraz',
     version='0.9',
+    author='Andrey Vlasovskikh',
+    author_email='andrey.vlasovskikh@gmail.com',
+    description='Static blog-aware site generator in Python mostly compatible '
+                'with Jekyll',
+    long_description=long_description,
+    url='http://obraz.pirx.ru/',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+    ],
     py_modules=['obraz'],
     install_requires=['PyYAML', 'Jinja2', 'Markdown', 'docopt'],
     entry_points={
@@ -25,11 +42,4 @@ setup(
     },
     data_files=[(os.path.join('obraz', root), files)
                 for root, files
-                in all_data_files('scaffold', excluded_dirs=['_site'])],
-    url='http://obraz.pirx.ru/',
-    license='MIT',
-    author='Andrey Vlasovskikh',
-    author_email='andrey.vlasovskikh@gmail.com',
-    description='Static site generator in a single Python file mostly '
-                'compatible with Jekyll',
-    long_description=long_description)
+                in all_data_files('scaffold', excluded_dirs=['_site'])])
